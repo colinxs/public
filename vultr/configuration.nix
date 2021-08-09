0c754015ae9ad5222bff1dbd696472362da6fedb
Colin Summers <me@colinxsummers.com>
2021-08-08 19:16:39 -0700

    save

---
+++ vultr/configuration.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ./configuration-extra.nix ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/vda";
+
+  networking.hostName = "nixos";
+
+  time.timeZone = "UTC";
+
+  networking.interfaces.enp1s0.useDHCP = true;
+  networking.interfaces.enp6s0.useDHCP = true;
+
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable sudo
+  };
+
+  environment.systemPackages = with pkgs; [
+    # Basic
+    coreutils
+    diffutils # For `cmp` and `diff`.
+    findutils
+    gnugrep
+    gnused
+    ncurses # For `tput`.
+    htop
+
+    # Archivers/Compression
+    gnutar
+    zstd
+    unzip
+    p7zip
+
+    # Networking
+    nmap
+    iftop
+    gitAndTools.gh # GitHub CLI
+  ];
+}
---
+++ vultr/setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Run with:
+# cd "$(curl -s -L https://github.com/colinxs/public/archive/master.tar.gz | tar -xvz | head -1)" && sudo ./nixos-vultr.sh
+
+set -ev
+
+DIR="$(dirname "$0")"
+
+DRIVE=/dev/vda
+
+BOOT_MB=512
+
+MIN_MEM_MB=4096
+MEM_MB=$(free -m | awk '/^Mem:/{print $2}')
+SWAP_MB=$([ $MEM_MB -lt $MIN_MEM_MB ] && echo $MIN_MEM_MB || echo $MEM_MB)
+
+# # Create GPT partition table
+# parted $DRIVE -- mklabel gpt
+#
+# # Create swap partition
+# parted $DRIVE -- mkpart primary ${BOOT_MB}MiB -${SWAP_MB}MiB
+# parted $DRIVE -- mkpart primary linux-swap -${SWAP_MB}MiB 100%
+#
+# # Create boot partition
+# parted $DRIVE -- mkpart ESP fat32 1MiB ${BOOT_MB}MiB
+# parted $DRIVE -- set 3 esp on
+#
+# # Format primary partition
+# mkfs.ext4 -L nixos ${DRIVE}1
+# ls /dev/disk/by-label
+#
+# # Create swap
+# mkswap -L swap ${DRIVE}2
+#
+# # Format boot
+# mkfs.fat -F 32 -n boot ${DRIVE}3
+#
+# # Mount target file system
+# mount /dev/disk/by-label/nixos /mnt
+#
+# # Mount boot
+# mkdir -p /mnt/boot
+# mount /dev/disk/by-label/boot /mnt/boot
+#
+# # Activate swap
+# swapon ${DRIVE}2
+
+mkdir -p /mnt/etc/nixos
+cp "${DIR}/configuration.nix" /mnt/etc/nixos
+cp "${DIR}/configuration-extra.nix" /mnt/etc/nixos
+
+nixos-install --no-root-passwd
