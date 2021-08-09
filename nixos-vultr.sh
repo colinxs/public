931ac0412367e440de5eadf698a81fa0c536e497
Colin Summers <me@colinxsummers.com>
2021-08-08 18:44:06 -0700

    save

--- nixos-vultr.sh
+++ nixos-vultr.sh
@@ -14,15 +14,15 @@ MEM_MB=$(free -m | awk '/^Mem:/{print $2}')
 SWAP_MB=$([ $MEM_MB -lt $MIN_MEM_MB ] && echo $MIN_MEM_MB || echo $MEM_MB)
 
 # Create GPT partition table
-sudo parted $DRIVE -- mklabel gpt
+parted $DRIVE -- mklabel gpt
 
 # Create swap partition
-sudo parted $DRIVE -- mkpart primary ${BOOT_MB}MiB -${SWAP_MB}MiB
-sudo parted $DRIVE -- mkpart primary linux-swap -${SWAP_MB}MiB 100%
+parted $DRIVE -- mkpart primary ${BOOT_MB}MiB -${SWAP_MB}MiB
+parted $DRIVE -- mkpart primary linux-swap -${SWAP_MB}MiB 100%
 
 # Create boot partition
-sudo parted $DRIVE -- mkpart ESP fat32 1MiB ${BOOT_MB}MiB
-sudo parted $DRIVE -- set 3 esp on
+parted $DRIVE -- mkpart ESP fat32 1MiB ${BOOT_MB}MiB
+parted $DRIVE -- set 3 esp on
 
 # Format primary partition
 mkfs.ext4 -L nixos ${DRIVE}1
