fc871e75466f1ae5d3e675bf1ebad88363df0c1a
Colin Summers <me@colinxsummers.com>
2021-10-15 17:14:15 -0600

    save

--- aws/main.sh
+++ aws/main.sh
@@ -4,4 +4,8 @@ git -C ~/public pull || git clone https://github.com/colinxs/public.git ~/public
 . ~/public/generic/nix.sh
 . ~/public/generic/docker.sh
 . ~/public/generic/kernel.sh
+# if ! apt list --installed | grep "linux-aws-edge"; then
+#     sudo apt install linux-aws-edge
+#     sudo shutdown -r now
+# fi
 CXS_DEBUG=1 MACHINE=aws1 "$(which nix)" run 'github:colinxs/bigdata#main'
