6c4b666c55bd54be0f48a9b771cebfb25f5ea38d
Colin Summers <me@colinxsummers.com>
2021-10-15 17:09:16 -0600

    save

--- aws/main.sh
+++ aws/main.sh
@@ -1,9 +1,7 @@
 sudo apt update
 sudo apt install msr-tools vim git -y
 git -C ~/public pull || git clone https://github.com/colinxs/public.git ~/public
-~/public/generic/nix.sh
-~/public/generic/docker.sh
-~/public/generic/kernel.sh
-~/public/aws/main.sh
+. ~/public/generic/nix.sh
+. ~/public/generic/docker.sh
+. ~/public/generic/kernel.sh
 CXS_DEBUG=1 MACHINE=aws1 "$(which nix)" run 'github:colinxs/bigdata#main'
-
