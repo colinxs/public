0b3a3523cd90a68125eb210bbc5caa420a73c1fd
Colin Summers <me@colinxsummers.com>
2021-10-16 20:03:21 -0600

    save

--- aws/main.sh
+++ aws/main.sh
@@ -5,14 +5,14 @@ sudo apt install coreutils msr-tools vim git -y
 
 git -C ~/public pull || git clone https://github.com/colinxs/public.git ~/public
 
-eval "$(~/public/generic/nix.sh)"
-eval "$(~/public/generic/docker.sh)"
+# eval "$(~/public/generic/nix.sh)"
+# eval "$(~/public/generic/docker.sh)"
 eval "$(~/public/generic/kernel.sh)"
 
 # TODO
-if ! apt list --installed | grep "linux-aws-edge"; then
-    sudo apt install linux-aws-edge
-    sudo shutdown -r now
-fi
+# if ! apt list --installed | grep "linux-aws-edge"; then
+#     sudo apt install linux-aws-edge
+#     sudo shutdown -r now
+# fi
 
 CXS_DEBUG=1 MACHINE=aws1 "$(command -v nix)" run 'github:colinxs/bigdata#main'
