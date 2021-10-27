6606b6be849972d9a1ddd8c52505bcc1a58f882e
Colin Summers <me@colinxsummers.com>
2021-10-27 02:16:08 -0600

    save

--- aws/main-stub.sh
+++ aws/main-stub.sh
@@ -2,11 +2,11 @@ eval "$(~/public/generic/nix.sh)"
 eval "$(~/public/generic/docker.sh)"
 eval "$(~/public/generic/service.sh)"
 
-eval "$(~/public/generic/kernel.sh)"
-sudo apt install linux-aws-edge -y
-sudo shutdown -r now
-
 if ! grep "access-tokens" /etc/nix/nix.conf; then
-    echo "access-tokens = github.com=<TOKEN_HERE>" | sudo tee -a /etc/nix/nix.conf 
+    echo "access-tokens = github.com=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" | sudo tee -a /etc/nix/nix.conf 
 fi
+
+eval "$(~/public/generic/kernel.sh)"
+# sudo apt install linux-aws-edge -y
+# sudo shutdown -r now
 
