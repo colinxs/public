a97c1605d5d4934f12779609d3a93745a5e0cd42
Colin Summers <me@colinxsummers.com>
2021-10-28 18:16:16 -0600

    save

--- aws/main.sh
+++ aws/main.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-GITHUB_TOKEN=""
-export GITHUB_TOKEN
+set +o noclobber
+
+script="$(mktemp)"
+sudo tee "$script" > /dev/null <<EOF
+export HOME=/home/ubuntu
 
 set -ex
 set +o noclobber
+
+echo "USER: $(whoami)"
+echo "HOME: ${HOME}"
+echo "DIR: $(pwd)"
+
+GITHUB_TOKEN=""
+export GITHUB_TOKEN
 
 sudo apt update
 sudo apt install coreutils msr-tools vim git -y
 
 git -C ~/public pull || git clone https://github.com/colinxs/public.git ~/public
 . ~/public/aws/main-stub.sh
+EOF
 
+sudo -i -u ubuntu bash "$script"
