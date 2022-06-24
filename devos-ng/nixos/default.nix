4047ad847e58effe812417711c8099ba327f1fbb
Colin Summers <me@colinxsummers.com>
2022-06-24 02:27:30 -0600

    save

---
+++ devos-ng/nixos/default.nix
@@ -0,0 +1,24 @@
+{ self, inputs, ... }:
+
+let
+  inherit (inputs.digga.lib) allProfilesTest;
+in
+
+{
+  hostDefaults = {
+    channelName = "nixos";
+  };
+
+  hosts = {
+    Morty = {
+      modules = [ ./Morty.nix ];
+      tests = [ allProfilesTest ];
+    };
+  };
+
+  importables = rec {
+    suites = rec {
+      base = [ ];
+    };
+  };
+}
