428223e1f6f676bfe5c838ef15b9c6d571942526
Colin Summers <m@lyres.org>
2022-12-15 07:41:57 -0700

    save

--- cfg/setup-fedora.sh
+++ cfg/setup-fedora.sh
@@ -20,27 +20,42 @@ enable-service() {
 
 
 # Util
-install util-linux-user ripgrep fd-find
+install util-linux-user ripgrep fd-find gparted htop
 
 # Kernel
-sudo grubby --update-kernel ALL --args "intel_iommu=on amd_iommu=on iommu=pt mitigations=off selinux=0"
+sudo grubby --update-kernel ALL \
+	--args "intel_iommu=on amd_iommu=on iommu=pt mitigations=off selinux=0" \
+        --remove-args "rhgb quiet"
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # Sudo
-read -p "Change 'Defaults' line in /etc/sudoers to: 'Defaults env_reset, timestamp_timeout=60'"
+read -p "Add to /etc/sudoers: 'Defaults timestamp_timeout=60'"
 
 # Shell
 install zsh
-chsh -s "$(which zsh)"
+# chsh -s "$(which zsh)"
 
 sudo dnf group install -y \
+	'Administration Tools' \
 	'Container Management' \
+	'Domain Membership' \
 	'Headless Management' \
-	'Network Servers'
+	'Network Servers' \
+	'System Tools'
 
 install firewall-config
 firewall-add mdns dhcpv6-client dns http http3 https
 
+# Pop Shell
+install gnome-shell-extension-pop-shell
+
+
+cat <<EOF | sudo tee /etc/cockpit/cockpit.conf
+[WebService]
+Origins =  http://localhost http://$(hostname) http://$(hostname).home.arpa http://$(hostname).cat-coho.ts.net http://$(hostname).lyres.org https://localhost https://$(hostname) https://$(hostname).home.arpa https://$(hostname).cat-coho.ts.net https://$(hostname).lyres.org gparted
+ProtocolHeader = X-Forwarded-Proto
+EOF
+read -p "Edit cockpit port to 80 or 443 in cockpit.socket"
 install \
 	cockpit \
 	cockpit-composer \
@@ -57,7 +72,6 @@ install \
 	cockpit-storaged \
 	cockpit-system
 firewall-add cockpit
-read -p "Edit cockpit port to 80 or 443 in cockpit.socket"
 enable-service cockpit.socket
 
 install neovim
@@ -69,13 +83,6 @@ install git
 sudo git config --global init.defaultBranch main
 git config --global init.defaultBranch main
 
-sudo dnf install etckeeper
-sudo etckeeper init
-(cd /etc && sudo git remote add origin git@github.com:colinxs/goliath-etc.git)
-sudo etckeeper commit "init"
-(cd /etc && sudo git push --set-upstream origin main)
-sudo systemctl enable --now etckeeper.timer
-
 sudo rpm --import https://kopia.io/signing-key
 cat <<EOF | sudo tee /etc/yum.repos.d/kopia.repo
 [Kopia]
@@ -86,6 +93,36 @@ enabled=1
 gpgkey=https://kopia.io/signing-key
 EOF
 install kopia kopia-ui
+sudo mkdir -p /var/cache/kopia
+sudo mkdir -p /var/log/kopia
+cat <<EOF | sudo tee /etc/systemd/system/kopia.service
+[Unit]
+Description=Kopia Server
+After=syslog.target
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/kopia server \
+	--log-dir=/var/log/kopia \
+	--cache-directory=/var/cache/kopia \
+	--content-cache-size-mb=$((256 * 1024)) \
+	--metadata-cache-size-mb=$((32 * 1024)) \
+	--tls-generate-cert \
+	--tls-cert-file=/var/lib/kopia/server.cert \
+	--tls-key-file=/var/lib/kopia/server.key \
+	--address=0.0.0.0:51515 \
+	--refresh-interval=60s \
+	--insecure
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+WantedBy=graphical.target
+# and adjust paths
+EOF
+enable-service kopia.service
 
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 cat <<EOF | sudo tee /etc/yum.repos.d/1password.repo
@@ -99,27 +136,30 @@ gpgkey="https://downloads.1password.com/linux/keys/1password.asc"
 EOF
 install 1password 1password-cli
 
+sudo dnf install etckeeper
+sudo etckeeper init
+(cd /etc && sudo git remote add origin "git@github.com:colinxs/$(hostname)-etc.git":)
+sudo etckeeper commit "init"
+(cd /etc && sudo git push --set-upstream origin main)
+sudo systemctl enable --now etckeeper.timer
+
+
 install tuned tuned-gtk
-enable-service tuned
+enable-service tuned.service
+
 
 enable-service dnf-makecache.timer
-
-cat <<EOF | sudo tee /etc/cockpit/cockpit.conf
-[WebService]
-AllowUnencrypted = true
-Origins = http://localhost https://localhost https://$(hostname).lyres.org
-ProtocolHeader = X-Forwarded-Proto
-ForwardedForHeader = X-Forwarded-For
-EOF
 
 # File Sharing
 sudo mkdir -p /share
 install samba wsdd
-firewall-add samba samba-dc samba-client wsdd wsdd-http ws-discovery ws-discovery-client ws-discovery-tcp ws-discovery-udp
+firewall-add \
+	samba samba-dc samba-client \
+	ws-discovery ws-discovery-client ws-discovery-tcp ws-discovery-udp
 enable-service smb.service wsdd.service
 
-install https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2022.12.05-1.fedora.x86_64.rpm
-firewall-add dropbox-lansync
+#install https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2022.12.05-1.fedora.x86_64.rpm
+#firewall-add dropbox-lansync
 
 # File Management
 install nemo
@@ -133,7 +173,6 @@ install gnome-console && sudo dnf remove gnome-terminal
 
 # Virtual Machines
 install virt-manager libvirt-daemon-kvm libvirt-daemon-lxc 
-
 
 # Grafana Agent
 cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
@@ -159,26 +198,5 @@ install syncthing
 firewall-add syncthing syncthing-gui
 enable-service syncthing@root.service
 
-install tigervnc-server
-cat <<EOF | sudo tee /etc/systemd/system/vncserver@.service
-[Unit]
-Description=Remote desktop service (VNC)
-After=syslog.target network.target
-
-[Service]
-Type=forking
-ExecStartPre=+/usr/libexec/vncsession-restore %i
-ExecStart=/sbin/runuser -l $USER -c "/usr/bin/vncserver %i -geometry 1920x1080"
-PIDFile=/home/$USER/.vnc/%H%i.pid
-SELinuxContext=system_u:system_r:vnc_session_t:s0
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo systemctl daemon-reload
-echo "Set VNC Pass:"
-vncpasswd
-enable-service vncserver@:0.service
-
-
-
+# Must be last
+sudo systemctl disable --now firewalld
